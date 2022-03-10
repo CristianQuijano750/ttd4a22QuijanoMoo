5e5ab198f6a34f0a31d5c82d9f015face1057ba4
CristianQuijano750 <91923330+CristianQuijano750@users.noreply.github.com>
2022-03-09 21:11:13 -0600

    Initial commit

---
+++ public/js/apis/apipruebas.js
@@ -0,0 +1,7 @@
+new Vue({
+el: '#vm',
+data:{
+	imagen:'https://play-lh.googleusercontent.com/Xea8QlyRQeWkInv0kilkYVxYCFgxPYAKjv9HXOqwObOassTyK5Nw22yR135OUQbhaA/300/200/'
+}
+
+	)}
---
+++ resources/views/layouts/pruebas.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>panas</title>
+</head>
+<body>
+
+<div id="vm">
+
+	<img v-bind:src="imagen">
+</div>
+
+<script src= "apipruebas.js"></script>
+
+</body>
+</html>
